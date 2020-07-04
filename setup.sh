6ce2009c532c075b1dd8ce6aa03fe22b30943a92
cilarp <cilarp914@gmail.com>
2020-07-05 08:17:09 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+echo "move to root..."
+sudo su
+
+#Change repojitory and update
+echo "change directory to japanese"
+sed -i.bak -e "s/http:\/\/archive\.ubuntu\.com/http:\/\/jp\.archive\.ubuntu\.com/g" /etc/apt/sources.list
+apt update && sudo apt upgrade
+
+
+#install essential
+apt install -y build-essential
+
+#git setup
+echo "set up git"
+add-apt-repository ppa:git-core/ppa
+apt update; apt install git
+git config --global user.name "cilarp"
+git config --global user.email "cilarp914@gmail.com"
+echo "You still need to set up ssh keys... Don't forget"
+
+#mono setup
+echo "set up mono"
+apt install gnupg ca-certificates
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+apt update
+apt install mono-devel
+
+#vim set up
+echo "set up vim"
+apt install -y ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev
+cd /opt/
+git clone https://github.com/vim/vim
+cd vim/
+./configure --with-features=huge --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-cscope --enable-fontset --enable-fail-if-missing --prefix=/usr/local --enable-pythoninterp=dynamic --enable-python3interp=dynamic --enable-rubyinterp=dynamic 
+make
+make install
+cd
+
+#neovim setup
+echo "set up neovim"
+apt install -y python3-pip m4 cmake automake unzip gettext
+pip3 install pynvim
+cd /opt/
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make CMAKE_BUILD_TYPE=Release
+make install
+cd
+
+
+#install rust and setup
+echo "set up rust"
+apt install -y curl codelldb
+exit
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source ~/.cargo/env
+rustup component add rls rust-analysis rust-src
+cargo install exa hexyl procs fd-find ripgrep bat
+echo "alias ils='exa'" >> ~/.bashrc
+echo "alias ila='exa -a'" >> ~/.bashrc
+echo "alias ill='exa -ahl --git'" >> ~/.bashrc 
+echo "alias icat='bat'"  >> ~/.bashrc 
+echo "alias iod='hexyl'" >> ~/.bashrc 
+echo "alias ips='procs'" >> ~/.bashrc 
+echo "alias ifind='fd'" >> ~/.bashrc 
+echo "alias igrep='rg'" >> ~/.bashrc 
+sudo su
+
+#install java11
+echo "set up java"
+apt install openjdk-11-jdk
+
+#install llvm
+echo "set up llvm"
+bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+#install julia 1.0.5-lts
+echo "set up julia"
+cd /opt
+wget https://julialang-s3.julialang.org/bin/linux/x64/1.0/julia-1.0.5-linux-x86_64.tar.gz
+tar -xvzf julia-1.0.5-linux-x86\_64.tar.gz
+export PATH="$PATH:/opt/julia-1.0.5/bin"
+
+exit
+ 
