c433a09349fac540e19cd0902a211920d55a3e44
cilarp <66499580+cilarp@users.noreply.github.com>
2020-07-05 09:29:54 +0900

    update

--- setup_wsl.sh
+++ setup_wsl.sh
@@ -3,5 +3,7 @@ bash ./setup.sh
 cd
 dircolors -p > .dircolors
 echo "please set up your dircolor"
+vim .dircolors
 echo "eval $(dircolors -b ~/.dircolors)" >> .bashrc
+source .bashrc
 ln -s /mnt/d/dev/ dev
