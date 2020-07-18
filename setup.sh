e13fbad9698ea006c50fce29537f4913b3850e0b
cilarp <66499580+cilarp@users.noreply.github.com>
2020-07-18 17:03:46 +0900

    update

--- setup.sh
+++ setup.sh
@@ -117,3 +117,11 @@ wget https://go.microsoft.com/fwlink/?LinkID=760868
 wget https://discord.com/api/download?platform=linux&format=deb
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.7.0-amd64.deb
 wget https://file-fevwnujbqw.now.sh/Chirp-linux-x64.zip
+
+"setup ssh
+ssh-keygen -t rsa -b 4096 -C "cilarp914@gmail.com"
+chmod 600 ~/.ssh/id_rsa_github
+echo "Host github"                         >> ~/.ssh/config
+echo "  HostName github.com"               >> ~/.ssh/config
+echo "  User git"                          >> ~/.ssh/config
+echo "  IdentityFile ~/.ssh/id_rsa_github" >> ~/.ssh/config
