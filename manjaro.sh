8ccad15699cb35e579021749633a4002ed8a84a9
cilarp <cilarp914@gmail.com>
2020-07-23 18:11:21 +0900

    update

---
+++ manjaro.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+sudo pacman -Syu
+
+#install essential
+sudo pacman -S base-devel yay snapd git
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+#install go
+sudo pacman -S go
+export GOPATH=~/workspace/go/
+
+#install vscode and git client
+sudo snap install code
+sudo snap install gitkraken --classic
+
+#install neovim
+pip3 install pynvim
+sudo pacman -S neovim
+
+#install browser
+sudo pacman -S chromium 
+
+#install nodejs
+sudo pacman -S nodejs npm
+
+#install docker
+sudo pacman -S docker
+
+#install wine 
+wine winetricks
+
+#install util
+sudo pacman -S evince atool rhythmbox neofetch plank
+git clone https://aur.archlinux.org/ulauncher.git && cd ulauncher && makepkg -is
+
+#install discord
+sudo pacman -S discord
+
+#install llvm
+cd /opt
+sudo wget https://github.com/llvm/llvm-project/releases/download/llvmorg-10.0.0/clang+llvm-10.0.0-aarch64-linux-gnu.tar.xz
+sudo tar -xvzf ./clang+llvm-10.0.0-aarch64-linux-gnu.tar.xz
+export PATH=$PATH:/opt/clang+llvm-10.0.0-aarch64-linux-gnu/bin
+
+#install latex
+sudo pacman -S texlive-langjapanese texlive-most texlive-latexextra
+
+#util
+bash ./common.sh
--- ubuntu18.sh
+++ ubuntu18.sh
@@ -8,11 +8,11 @@ sudo apt update && sudo apt upgrade -y
 
 
 
-"add ppa
+#add ppa
 sudo add-apt-repository ppa:noobslab/macbuntu
 sudo add-apt-repository ppa:git-core/ppa
 
-"apt install
+#apt install
 sudo apt install -y neofetch
 sudo apt install -y build-essential
 sudo apt install -y ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev 
@@ -24,12 +24,6 @@ sudo apt install -y openjdk-11-jdk
 sudo apt install -y texlive-full
 sudo apt install -y macbuntu-os-icons-v1804 macbuntu-os-ithemes-v1804 slingscold albert macbuntu-os-plank-theme-v1804
 sudo apt isntall -y python3-setuptools python-setuptools
-
-#git: setup
-echo "set up git"
-git config --global user.name "cilarp"
-git config --global user.email "cilarp914@gmail.com"
-echo "You still need to set up ssh keys... Don't forget"
 
 #mono: install and setup
 echo "set up mono"
@@ -58,47 +52,6 @@ sudo make CMAKE_BUILD_TYPE=Release
 sudo make install
 cd
 
-
-#rust: install and setup
-echo "set up rust"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source ~/.cargo/env
-rustup component add rls rust-analysis rust-src
-cargo install exa hexyl procs fd-find ripgrep bat
-echo "alias ils='exa'"             >> ~/.bashrc
-echo "alias ila='exa -a'"          >> ~/.bashrc
-echo "alias ill='exa -ahl --git'"  >> ~/.bashrc 
-echo "alias icat='bat'"            >> ~/.bashrc 
-echo "alias iod='hexyl'"           >> ~/.bashrc 
-echo "alias ips='procs'"           >> ~/.bashrc 
-echo "alias ifind='fd'"            >> ~/.bashrc 
-echo "alias igrep='rg'"            >> ~/.bashrc 
-
-
-#install llvm
-echo "llvm: install"
-sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
-
-
- 
-#install powerline-shell
-cd /opt
-sudo git clone https://github.com/b-ryan/powerline-shell
-cd powerline-shell
-python setup.py install
-echo "function _update_ps1() {"                                           >> ~/.bashrc
-echo "    PS1=$(powerline-shell $?)"                                      >> ~/.bashrc
-echo "}"                                                                  >> ~/.bashrc
-echo ""                                                                   >> ~/.bashrc
-echo "if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then"  >> ~/.bashrc
-echo "    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND""                  >> ~/.bashrc
-echo "fi"                                                                 >> ~/.bashrc
-cd /opt
-sudo git clone https://github.com/powerline/fonts
-cd fonts
-sudo ./install.sh
-
-
 #install software
 cd ~/Downloads
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
@@ -107,11 +60,9 @@ wget https://discord.com/api/download?platform=linux&format=deb
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.7.0-amd64.deb
 wget https://file-fevwnujbqw.now.sh/Chirp-linux-x64.zip
 
+#install llvm
+echo "llvm: install"
+sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
-"setup ssh
-ssh-keygen -t rsa -b 4096 -C "cilarp914@gmail.com"
-chmod 600 ~/.ssh/id_rsa_github
-echo "Host github"                         >> ~/.ssh/config
-echo "  HostName github.com"               >> ~/.ssh/config
-echo "  User git"                          >> ~/.ssh/config
-echo "  IdentityFile ~/.ssh/id_rsa_github" >> ~/.ssh/config
+cd
+bash ./common.sh
