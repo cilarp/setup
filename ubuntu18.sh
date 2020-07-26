3ffa090e5deede3fa3cc9d96cfe50327eabad347
cilarp <cilarp914@gmail.com>
2020-07-26 18:41:22 +0900

    rename

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -2,27 +2,27 @@
 origin=$(pwd)
 
 #Change repojitory and update
-bash ./ubuntu/ch_repo_to_jp.sh
+bash ./ubuntu18/ch_repo_to_jp.sh
 
 #apt install
 sudo apt install -y lldb 
 
 #install essential
 cd $origin
-bash ./ubuntu/essential.sh
+bash ./ubuntu18/essential.sh
 
 #git: install and setting
 cd $origin
-bash ./ubuntu/git.sh
+bash ./ubuntu18/git.sh
 bash ./common/git.sh
 
 #install llvm
 cd $origin
-bash ./ubuntu/llvm.sh
+bash ./ubuntu18/llvm.sh
 
 #mono: install and setup
 cd $origin
-bash ./ubuntu/mono.sh
+bash ./ubuntu18/mono.sh
 
 #rust: install and setup
 cd $origin
@@ -31,11 +31,11 @@ bash ./common/rust.sh
 
 #vim: install and set up
 cd $origin
-bash ./ubuntu/vim.sh
+bash ./ubuntu18/vim.sh
 
 #neovim: instal and setup
 cd $origin
-bash ./ubuntu/nvim.sh
+bash ./ubuntu18/nvim.sh
 
 #java: install
 cd $origin
@@ -43,7 +43,7 @@ sudo apt install -y openjdk-11-jdk
 
 #latex: install and setup
 cd $origin
-bash ./ubuntu/latex.sh
+bash ./ubuntu18/latex.sh
 
 #install powerline
 sudo apt install -y python3-setuptools python-setuptools
