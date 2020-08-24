e0f7b7fe464db0ac4325cc7ee3641794f9eda4e1
cilarp <66499580+cilarp@users.noreply.github.com>
2020-08-24 20:44:31 +0900

    Update ubuntu18.sh

--- ubuntu18.sh
+++ ubuntu18.sh
@@ -4,8 +4,10 @@ origin=$(pwd)
 #Change repojitory and update
 bash ./ubuntu18/ch_repo_to_jp.sh
 
-#apt install
-sudo apt install -y lldb 
+read -n1 -p "ok? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    sudo apt install -y lldb 
+fi
 
 #install essential
 cd $origin
@@ -17,38 +19,63 @@ bash ./ubuntu18/git.sh
 bash ./common/git.sh
 
 #llvm: install
+read -n1 -p "do u wanna install mono? (y/N): " yn
 cd $origin
-bash ./ubuntu18/llvm.sh
+if [[ $yn = [yY] ]]; then
+    bash ./ubuntu18/llvm.sh
+fi
 
 #mono: install and setup
+read -n1 -p "do u wanna install mono? (y/N): " yn
 cd $origin
-bash ./ubuntu18/mono.sh
+if [[ $yn = [yY] ]]; then
+    bash ./ubuntu18/mono.sh
+fi
 
 #rust: install and setup
+read -n1 -p "do u wanna install rust? (y/N): " yn
 cd $origin
-sudo apt install -y curl
-bash ./common/rust.sh
+if [[ $yn = [yY] ]]; then
+    sudo apt install -y curl
+    bash ./common/rust.sh
+fi
+
 
 #vim: install and set up
+read -n1 -p "do u wanna install vim? (y/N): " yn
 cd $origin
-bash ./ubuntu18/vim.sh
+if [[ $yn = [yY] ]]; then
+    bash ./ubuntu18/vim.sh
+fi
 
 #neovim: instal and setup
+read -n1 -p "do u wanna install neovim? (y/N): " yn
 cd $origin
-bash ./ubuntu18/nvim.sh
+if [[ $yn = [yY] ]]; then
+    bash ./ubuntu18/nvim.sh
+fi
 
 #java: install
+read -n1 -p "do u wanna install java? (y/N): " yn
 cd $origin
-sudo apt install -y openjdk-11-jdk
+if [[ $yn = [yY] ]]; then
+    sudo apt install -y openjdk-11-jdk
+fi
 
 #latex: install and setup
+read -n1 -p "do u wanna install latex? (y/N): " yn
 cd $origin
-bash ./ubuntu18/latex.sh
+if [[ $yn = [yY] ]]; then
+    bash ./ubuntu18/latex.sh
+fi
 
 #powerline: install
-sudo apt install -y python3-setuptools python-setuptools
+read -n1 -p "do u wanna install powerline-shell? (y/N): " yn
 cd $origin
-bash ./common/powerline.sh
+if [[ $yn = [yY] ]]; then
+    sudo apt install -y python3-setuptools python-setuptools
+    bash ./common/powerline.sh
+fi
 
 #ssh: setup
 cd $origin
