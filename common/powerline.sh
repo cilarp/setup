8b3f1e01f9db8650da633d2b46010fe88105211f
cilarp <66499580+cilarp@users.noreply.github.com>
2020-08-26 18:54:06 +0900

    Update powerline.sh

--- common/powerline.sh
+++ common/powerline.sh
@@ -1,14 +1,9 @@
+setupDir=$(pwd)
 cd /opt
 sudo git clone https://github.com/b-ryan/powerline-shell
 cd powerline-shell
 sudo python setup.py install
-echo "function _update_ps1() {"                                           >> ~/.bashrc
-echo "    PS1=$(powerline-shell $?)"                                      >> ~/.bashrc
-echo "}"                                                                  >> ~/.bashrc
-echo ""                                                                   >> ~/.bashrc
-echo "if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then"  >> ~/.bashrc
-echo "    PROMPT_COMMAND=\"_update_ps1; $PROMPT_COMMAND\""                    >> ~/.bashrc
-echo "fi"                                                                 >> ~/.bashrc
+cat $setupDir/powerline_bash | echo >> ~/.bashrc
 cd /opt
 sudo git clone https://github.com/powerline/fonts
 cd fonts
