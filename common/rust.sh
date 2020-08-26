a176776b961f556af0915ef33dbaf38f73754df3
cilarp <66499580+cilarp@users.noreply.github.com>
2020-08-26 19:08:56 +0900

    Update rust.sh

--- common/rust.sh
+++ common/rust.sh
@@ -4,6 +4,7 @@ rustup component add rls rust-analysis rust-src
 sudo apt install -y libssl-dev
 
 read -n1 -p "do u wanna install exa? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install exa 
     echo "alias ils='exa'"             >> ~/.bashrc
@@ -12,47 +13,55 @@ if [[ $yn = [yY] ]]; then
 fi
 
 read -n1 -p "do u wanna install hexyl? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install hexyl 
     echo "alias iod='hexyl'"           >> ~/.bashrc 
 fi
 
 read -n1 -p "do u wanna install procs? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install procs 
     echo "alias ips='procs'"           >> ~/.bashrc 
 fi
 
 read -n1 -p "do u wanna install fd-find? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install fd-find 
     echo "alias ifind='fd'"            >> ~/.bashrc 
 fi
 
 read -n1 -p "do u wanna install ripgrep? (y/N): " yn
+echo "" 
 if [[ $yn = [yY] ]]; then
     cargo install ripgrep
     echo "alias igrep='rg'"            >> ~/.bashrc     
 fi
 
 read -n1 -p "do u wanna install bat? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install bat
     echo "alias icat='bat'"            >> ~/.bashrc 
 fi
 
 read -n1 -p "do u wanna install cargo-atcoder? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install cargo-atcoder
     mkdir ~/.cache/cargo-atcoder
 fi
 
 read -n1 -p "do u wanna install cargo-maker? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install cargo-make
 fi
 
 read -n1 -p "do u wanna install cargo-generate? (y/N): " yn
+echo ""
 if [[ $yn = [yY] ]]; then
     cargo install cargo-generate
 fi
