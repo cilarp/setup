48e16974a0f1affe702d1d485c909d7530b789eb
cilarp <66499580+cilarp@users.noreply.github.com>
2020-08-26 18:50:35 +0900

    Update rust.sh

--- common/rust.sh
+++ common/rust.sh
@@ -1,15 +1,60 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source ~/.cargo/env
 rustup component add rls rust-analysis rust-src
-cargo install exa hexyl procs fd-find ripgrep bat cargo-atcoder cargo-make cargo-generate
-echo "alias ils='exa'"             >> ~/.bashrc
-echo "alias ila='exa -a'"          >> ~/.bashrc
-echo "alias ill='exa -ahl --git'"  >> ~/.bashrc 
-echo "alias icat='bat'"            >> ~/.bashrc 
-echo "alias iod='hexyl'"           >> ~/.bashrc 
-echo "alias ips='procs'"           >> ~/.bashrc 
-echo "alias ifind='fd'"            >> ~/.bashrc 
-echo "alias igrep='rg'"            >> ~/.bashrc 
+sudo apt install -y libssl-dev
+
+read -n1 -p "do u wanna install exa? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install exa 
+    echo "alias ils='exa'"             >> ~/.bashrc
+    echo "alias ila='exa -a'"          >> ~/.bashrc
+    echo "alias ill='exa -ahl --git'"  >> ~/.bashrc 
+fi
+
+read -n1 -p "do u wanna install hexyl? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install hexyl 
+    echo "alias iod='hexyl'"           >> ~/.bashrc 
+fi
+
+read -n1 -p "do u wanna install procs? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install procs 
+    echo "alias ips='procs'"           >> ~/.bashrc 
+fi
+
+read -n1 -p "do u wanna install fd-find? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install fd-find 
+    echo "alias ifind='fd'"            >> ~/.bashrc 
+fi
+
+read -n1 -p "do u wanna install ripgrep? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install ripgrep
+    echo "alias igrep='rg'"            >> ~/.bashrc     
+fi
+
+read -n1 -p "do u wanna install bat? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install bat
+    echo "alias icat='bat'"            >> ~/.bashrc 
+fi
+
+read -n1 -p "do u wanna install cargo-atcoder? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install cargo-atcoder
+    mkdir ~/.cache/cargo-atcoder
+fi
+
+read -n1 -p "do u wanna install cargo-maker? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install cargo-make
+fi
+
+read -n1 -p "do u wanna install cargo-generate? (y/N): " yn
+if [[ $yn = [yY] ]]; then
+    cargo install cargo-generate
+fi
+
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-mkdir ~/.cache/cargo-atcoder
-touch ~/.cache/cargo-atcoder/setting.sh
